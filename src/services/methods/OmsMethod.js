adc8dce00661afc594f7ae18b175c6772e063a74
hasapu <hari.sakti.putra@gmail.com>
2020-01-08 16:45:15 +0700

    order

--- src/services/methods/OmsMethod.js
+++ src/services/methods/OmsMethod.js
@@ -1,5 +1,5 @@
 import ApiRest from '../apiRest';
-
+/** GET CART ITEM */
 function getCartItem(data) {
   return ApiRest({
     path: 'add-cart',
@@ -11,7 +11,35 @@ function getCartItem(data) {
     }
   });
 }
+/** GET PAYMENT */
+function getPayment(data) {
+  return ApiRest({
+    path: `payment-list?orderParcelId=${data.parcelId}`,
+    method: 'GET'
+  });
+}
+/** GET PAYMENT */
+function confirmOrder(data) {
+  return ApiRest({
+    path: 'confirm-order',
+    method: 'POST',
+    params: {
+      orderId: data.orderId,
+      parcels: data.parcels
+    }
+  });
+}
+/** DELETE ORDER */
+function deleteOrder(data) {
+  return ApiRest({
+    path: `orders/${data.orderId}`,
+    method: 'DELETE'
+  });
+}
 
 export const OmsMethod = {
-  getCartItem
+  getCartItem,
+  getPayment,
+  confirmOrder,
+  deleteOrder
 };
--- src/state/sagas/OmsSaga.js
+++ src/state/sagas/OmsSaga.js
@@ -2,8 +2,19 @@ import { put, call, takeEvery } from 'redux-saga/effects';
 import { OmsMethod } from '../../services/methods';
 import * as ActionCreators from '../actions';
 import * as types from '../types';
-/** === MERCHANT LIST === */
+/** === CART ITEM LIST === */
 function* getCartItem(actions) {
+  try {
+    const response = yield call(() => {
+      return OmsMethod.getCartItem(actions.payload);
+    });
+    yield put(ActionCreators.omsGetCartItemSuccess(response));
+  } catch (error) {
+    yield put(ActionCreators.omsGetCartItemFailed(error));
+  }
+}
+/** === CHECKOUT ITEM LIST === */
+function* getCheckoutItem(actions) {
   try {
     const response = yield call(() => {
       return OmsMethod.getCartItem(actions.payload);
@@ -16,6 +27,7 @@ function* getCartItem(actions) {
 /** === SAGA FUNCTION === */
 function* OmsSaga() {
   yield takeEvery(types.OMS_GET_CART_ITEM_PROCESS, getCartItem);
+  yield takeEvery(types.OMS_GET_CHECKOUT_ITEM_PROCESS, getCheckoutItem);
 }
 
 export default OmsSaga;
