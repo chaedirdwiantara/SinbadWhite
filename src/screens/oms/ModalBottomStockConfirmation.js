536d4d39b14b595a07644f21c80db7701db1e332
hasapu <hari.sakti.putra@gmail.com>
2020-03-30 17:29:04 +0700

    OMS -> change order flow to warehouse
    Former-commit-id: a6cf3880035b256c38d6caf92036f157d33a33c4

--- src/screens/oms/ModalBottomStockConfirmation.js
+++ src/screens/oms/ModalBottomStockConfirmation.js
@@ -1,67 +1,83 @@
 import React, { Component } from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-  Dimensions,
-  StatusBar,
-  Image,
-  TouchableOpacity,
-  ScrollView
-} from 'react-native';
+import { View, StyleSheet, Dimensions, Image, ScrollView } from 'react-native';
+import Text from 'react-native-text';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import Modal from 'react-native-modal';
-import { Button } from 'react-native-elements';
-import { RFPercentage } from 'react-native-responsive-fontsize';
-import Fonts from '../../utils/Fonts';
-import * as ActionCreators from '../../state/actions';
+import Fonts from '../../helpers/GlobalFont';
 import ModalBottomType1 from '../../components/modal_bottom/ModalBottomType1';
 import { StatusBarRedOP50 } from '../../components/StatusBarGlobal';
+import masterColor from '../../config/masterColor.json';
+import GlobalStyles from '../../helpers/GlobalStyle';
 
 const { height } = Dimensions.get('window');
 
 class ModalBottomStockConfirmation extends Component {
+  /** === RENDER IMAGE === */
+  renderImage(item) {
+    return (
+      <View
+        style={{
+          paddingRight: 16,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Image
+          defaultSource={require('../../assets/images/sinbad_image/sinbadopacity.png')}
+          source={{
+            uri: item.catalogue.catalogueImages[0].imageUrl
+          }}
+          style={[GlobalStyles.image54Contain, { opacity: 0.5 }]}
+        />
+      </View>
+    );
+  }
+  /** === RENDER SKU NAME === */
+  renderSkuName(item) {
+    return (
+      <View>
+        <Text
+          style={[
+            Fonts.type16,
+            {
+              opacity: item.errorCode === 'ERR-STOCK' ? 1 : 0.5,
+              textTransform: 'capitalize'
+            }
+          ]}
+        >
+          {item.catalogue.name}
+        </Text>
+      </View>
+    );
+  }
+  /** === RENDER SKU INFO === */
+  renderSkuInfo(info) {
+    return (
+      <View style={{ marginTop: 8 }}>
+        <Text style={[Fonts.type8, { opacity: 0.5 }]}>{info}</Text>
+      </View>
+    );
+  }
+  /** === RENDER CHANGE STOCK === */
   renderPerubahanStockContent() {
     return this.props.oms.errorOmsGetCheckoutItem.data.map((item, index) => {
       return item.errorCode === 'ERR-STOCK' ? (
-        <View style={styles.boxPerContent} key={index}>
-          <View
-            style={{
-              width: '25%',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <Image
-              defaultSource={require('../../assets/images/sinbad_image/sinbadopacity.png')}
-              source={{
-                uri: item.catalogue.catalogueImages[0].imageUrl
+        <View style={{ flexDirection: 'row' }} key={index}>
+          {this.renderImage(item)}
+          <View style={styles.boxNameAndInfo}>
+            {this.renderSkuName(item)}
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: 8
               }}
-              style={styles.productImage}
-            />
-          </View>
-          <View
-            style={{
-              width: '40%',
-              justifyContent: 'space-between',
-              paddingVertical: 13
-            }}
-          >
-            <View>
-              <Text style={styles.productNameText}>{item.catalogue.name}</Text>
-            </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={styles.stockChangeText}>
-                {item.requestStock} pcs
-              </Text>
+            >
+              <Text style={Fonts.type23}>{item.requestStock} pcs</Text>
               <Image
                 source={require('../../assets/icons/global/arrow_right.png')}
                 style={{ height: 18, width: 18, marginHorizontal: 5 }}
               />
-              <Text style={styles.stockChangeText}>
-                {item.suggestedStock} pcs
-              </Text>
+              <Text style={Fonts.type23}>{item.suggestedStock} pcs</Text>
             </View>
           </View>
         </View>
@@ -70,46 +86,15 @@ class ModalBottomStockConfirmation extends Component {
       );
     });
   }
-
+  /** === RENDER BARANG HABIS === */
   renderProductErrorBarangHabis() {
     return this.props.oms.errorOmsGetCheckoutItem.data.map((item, index) => {
       return item.errorCode === 'ERR-RUN-OUT' ? (
-        <View style={styles.boxPerContent} key={index}>
-          <View
-            style={{
-              width: '25%',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <Image
-              defaultSource={require('../../assets/images/sinbad_image/sinbadopacity.png')}
-              source={{
-                uri: item.catalogue.catalogueImages[0].imageUrl
-              }}
-              style={[styles.productImage, { opacity: 0.5 }]}
-            />
-          </View>
-          <View
-            style={{
-              width: '40%',
-              justifyContent: 'space-between',
-              paddingVertical: 13
-            }}
-          >
-            <View>
-              <Text
-                style={[
-                  styles.productNameText,
-                  { color: 'rgba(51, 51, 51, 0.5)' }
-                ]}
-              >
-                {item.catalogue.name}
-              </Text>
-            </View>
-            <View>
-              <Text style={styles.stockError}>Stock Habis</Text>
-            </View>
+        <View style={{ flexDirection: 'row' }} key={index}>
+          {this.renderImage(item)}
+          <View style={styles.boxNameAndInfo}>
+            {this.renderSkuName(item)}
+            {this.renderSkuInfo('Stock Habis')}
           </View>
         </View>
       ) : (
@@ -117,46 +102,16 @@ class ModalBottomStockConfirmation extends Component {
       );
     });
   }
-
+  /** === RENDER PRODUCT TIDAK TERSEDIA === */
   renderProductErrorTidakTersedia() {
     return this.props.oms.errorOmsGetCheckoutItem.data.map((item, index) => {
-      return item.errorCode === 'ERR-STATUS' ? (
-        <View style={styles.boxPerContent} key={index}>
-          <View
-            style={{
-              width: '25%',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <Image
-              defaultSource={require('../../assets/images/sinbad_image/sinbadopacity.png')}
-              source={{
-                uri: item.catalogue.catalogueImages[0].imageUrl
-              }}
-              style={[styles.productImage, { opacity: 0.5 }]}
-            />
-          </View>
-          <View
-            style={{
-              width: '40%',
-              justifyContent: 'space-between',
-              paddingVertical: 13
-            }}
-          >
-            <View>
-              <Text
-                style={[
-                  styles.productNameText,
-                  { color: 'rgba(51, 51, 51, 0.5)' }
-                ]}
-              >
-                {item.catalogue.name}
-              </Text>
-            </View>
-            <View>
-              <Text style={styles.stockError}>Produk Tidak Tersedia</Text>
-            </View>
+      return item.errorCode === 'ERR-STATUS' ||
+        item.errorCode === 'ERR-WAREHOUSE' ? (
+        <View style={{ flexDirection: 'row' }} key={index}>
+          {this.renderImage(item)}
+          <View style={styles.boxNameAndInfo}>
+            {this.renderSkuName(item)}
+            {this.renderSkuInfo('Produk Tidak Tersedia')}
           </View>
         </View>
       ) : (
@@ -164,17 +119,17 @@ class ModalBottomStockConfirmation extends Component {
       );
     });
   }
-
+  /** === RENDER PRODUCT CHANGE STOCK === */
   renderPerubahanStock() {
     return this.props.oms.errorOmsGetCheckoutItem.data.find(
       item => item.errorCode === 'ERR-STOCK'
     ) !== undefined ? (
-      <View style={styles.boxHeaderContent}>
-        <View style={{ paddingTop: 10, paddingLeft: 10 }}>
-          <Text style={styles.subTitle}>Perubahan Stock</Text>
+      <View>
+        <View style={{ paddingTop: 10, paddingLeft: 16 }}>
+          <Text style={Fonts.type42}>Perubahan Stock</Text>
         </View>
-        <View style={styles.lines} />
-        <View style={{ paddingHorizontal: 10 }}>
+        <View style={[GlobalStyles.lines, { marginTop: 8, marginLeft: 16 }]} />
+        <View style={{ paddingHorizontal: 16 }}>
           {this.renderPerubahanStockContent()}
         </View>
       </View>
@@ -182,17 +137,17 @@ class ModalBottomStockConfirmation extends Component {
       <View />
     );
   }
-
+  /** === RENDER PRODUCT HABIS === */
   renderBarangHabis() {
     return this.props.oms.errorOmsGetCheckoutItem.data.find(
       item => item.errorCode === 'ERR-RUN-OUT'
     ) !== undefined ? (
-      <View style={styles.boxHeaderContent}>
-        <View style={{ paddingTop: 10, paddingLeft: 10 }}>
-          <Text style={styles.subTitle}>Barang Habis</Text>
+      <View>
+        <View style={{ paddingTop: 10, paddingLeft: 16 }}>
+          <Text style={Fonts.type42}>Barang Habis</Text>
         </View>
-        <View style={styles.lines} />
-        <View style={{ paddingHorizontal: 10 }}>
+        <View style={[GlobalStyles.lines, { marginTop: 8, marginLeft: 16 }]} />
+        <View style={{ paddingHorizontal: 16 }}>
           {this.renderProductErrorBarangHabis()}
         </View>
       </View>
@@ -200,17 +155,18 @@ class ModalBottomStockConfirmation extends Component {
       <View />
     );
   }
-
+  /** === RENDER PRODUCT TIDAK TERSEDIA === */
   renderProductTidakTersedia() {
     return this.props.oms.errorOmsGetCheckoutItem.data.find(
-      item => item.errorCode === 'ERR-STATUS'
+      item =>
+        item.errorCode === 'ERR-STATUS' || item.errorCode === 'ERR-WAREHOUSE'
     ) !== undefined ? (
-      <View style={styles.boxHeaderContent}>
-        <View style={{ paddingTop: 10, paddingLeft: 10 }}>
-          <Text style={styles.subTitle}>Product Tidak Tersedia</Text>
+      <View>
+        <View style={{ paddingTop: 10, paddingLeft: 16 }}>
+          <Text style={Fonts.type42}>Produk Tidak Tersedia</Text>
         </View>
-        <View style={styles.lines} />
-        <View style={{ paddingHorizontal: 10 }}>
+        <View style={[GlobalStyles.lines, { marginTop: 8, marginLeft: 16 }]} />
+        <View style={{ paddingHorizontal: 16 }}>
           {this.renderProductErrorTidakTersedia()}
         </View>
       </View>
@@ -218,13 +174,13 @@ class ModalBottomStockConfirmation extends Component {
       <View />
     );
   }
-
+  /** === RENDER CONTENT === */
   renderContent() {
     return (
       <View style={styles.container}>
         <StatusBarRedOP50 />
         <View style={{ paddingHorizontal: 10, paddingBottom: 10 }}>
-          <Text style={styles.titleDescription}>
+          <Text style={[Fonts.type59, { textAlign: 'center' }]}>
             Beberapa informasi produk pada pesanan Anda telah diperbaharui mohon
             tinjau ulang keranjang dan coba lagi
           </Text>
@@ -239,7 +195,7 @@ class ModalBottomStockConfirmation extends Component {
       </View>
     );
   }
-
+  /** === MAIN === */
   render() {
     return (
       <ModalBottomType1
@@ -256,93 +212,17 @@ class ModalBottomStockConfirmation extends Component {
 const styles = StyleSheet.create({
   container: {
     height: 0.6 * height,
-    backgroundColor: 'white',
+    backgroundColor: masterColor.backgroundWhite,
     flexDirection: 'column',
     width: '100%'
-  },
-  modalPosition: {
-    marginBottom: 0,
-    marginLeft: 0,
-    marginRight: 0
-  },
-  titleContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title: {
-    marginTop: 10,
-    fontFamily: Fonts.MontserratBold,
-    fontSize: RFPercentage(1.8),
-    color: '#333333'
-  },
-  subTitle: {
-    fontFamily: Fonts.MontserratSemiBold,
-    fontSize: RFPercentage(1.7),
-    color: '#333333'
-  },
-  productNameText: {
-    fontFamily: Fonts.MontserratSemiBold,
-    fontSize: RFPercentage(1.6),
-    color: '#333333',
-    lineHeight: 15
-  },
-  stockChangeText: {
-    fontFamily: Fonts.MontserratMedium,
-    fontSize: RFPercentage(1.6),
-    color: '#4f4f4f'
-  },
-  stockError: {
-    fontFamily: Fonts.MontserratMedium,
-    fontSize: RFPercentage(1.3),
-    color: 'rgba(79, 79, 79, 0.5)'
-  },
-  titleDescription: {
-    fontFamily: Fonts.MontserratMedium,
-    fontSize: RFPercentage(1.6),
-    color: '#333333',
-    lineHeight: 15,
-    textAlign: 'center'
   },
   contentContainer: {
     flex: 1
   },
-  lines: {
-    marginLeft: 10,
-    borderTopWidth: 1,
-    marginVertical: 10,
-    borderColor: '#f2f2f2'
-  },
-  productImage: {
-    width: 77,
-    height: undefined,
-    aspectRatio: 1 / 1
-  },
-  /**for content */
-  boxPerContent: {
-    flexDirection: 'row',
-    height: 0.11 * height
-  },
-  boxHeaderContent: {
-    flex: 1
-  },
-  /** for button */
-  buttonContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 10,
-    backgroundColor: '#ffffff'
-  },
-  titleButton: {
-    fontFamily: Fonts.MontserratBold,
-    fontSize: 13,
-    color: '#ffffff'
-  },
-  button: {
-    backgroundColor: '#f0444c',
-    borderRadius: 10,
-    width: 282,
-    height: 41
+  boxNameAndInfo: {
+    width: '60%',
+    justifyContent: 'space-between',
+    paddingVertical: 13
   }
 });
 
@@ -350,12 +230,5 @@ const mapStateToProps = ({ oms }) => {
   return { oms };
 };
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators(ActionCreators, dispatch);
-};
-
 // eslint-disable-next-line prettier/prettier
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ModalBottomStockConfirmation);
+export default connect(mapStateToProps, {})(ModalBottomStockConfirmation);
