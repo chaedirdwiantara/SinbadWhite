1bc456281dd79db6868b5373f30c61bc3bc49ec0
hasapu <hari.sakti.putra@gmail.com>
2020-04-21 02:07:48 +0700

    PDP -> add global function get supplier in pdp method
    Former-commit-id: a7b2dc6a6c6f5c8041ea3b46145d63c9b89a5125

--- src/services/methods/PdpMethod.js
+++ src/services/methods/PdpMethod.js
@@ -3,18 +3,18 @@ import { GlobalMethod } from '../methods/GlobalMethod';
 
 function getPdp(data) {
   return ApiRest({
-    path: `catalogues?$skip=${data.page}&$limit=10&supplierIds=${JSON.stringify(
-      data.supplierId
-    )}&searchName=${data.search}&status=active&sort=${data.sort}&sortby=${
-      data.sortBy
-    }`,
+    path: `catalogues?$skip=${
+      data.page
+    }&$limit=10&supplierIds=${GlobalMethod.merchantSupplierMapping()}&searchName=${
+      data.search
+    }&status=active&sort=${data.sort}&sortby=${data.sortBy}`,
     method: 'GET'
   });
 }
 /** GET PDP SKU DETAIL */
 function getDetailPdp(pdpId) {
   return ApiRest({
-    path: `catalogues/${pdpId}${GlobalMethod.userStoreUrban()}`,
+    path: `catalogues/${pdpId}?urbanId=${GlobalMethod.merchantStoreUrban()}&storeId=${GlobalMethod.merchantStoreId()}`,
     method: 'GET'
   });
 }
