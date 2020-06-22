d3326f475ea349f9ba6633069f0b95b2fb3d3faf
hasapu <hari.sakti.putra@gmail.com>
2020-06-22 15:24:28 +0700

    CORE HELPERS -> add index.js to helpers
    Former-commit-id: ae9caf008b1bbf315603bc34d11c4b4223d1eefd

---
+++ src/helpers/index.js
@@ -0,0 +1,13 @@
+import Fonts from './GlobalFont';
+import GlobalStyle from './GlobalStyle';
+import GlobalStyleHtml from './GlobalStyleHtml';
+import { NumberFormat, MoneyFormat, MoneyFormatShort } from './NumberFormater';
+
+export {
+  Fonts,
+  GlobalStyle,
+  GlobalStyleHtml,
+  NumberFormat,
+  MoneyFormat,
+  MoneyFormatShort
+};
