61581a29fc8f184158f2febbd590964edcb222ba
hasapu <hari.sakti.putra@gmail.com>
2020-06-22 15:26:02 +0700

    CORE FONTS -> add new fonts
    Former-commit-id: 0e6bc50b4385eb37022d16ebb5fc75a880f6de17

--- src/helpers/GlobalFont.js
+++ src/helpers/GlobalFont.js
@@ -1,613 +1,613 @@
-import Fonts from '../config/Fonts';
-import { StyleSheet } from 'react-native';
-import masterColor from '../config/masterColor.json';
+import { Fonts, Color } from '../config';
+import { StyleSheet } from '../library/reactPackage';
+import { RFPercentage } from '../library/thirdPartyPackage';
 
 const styles = StyleSheet.create({
   /** === regular text === */
   type1: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 40,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type2: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type3: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 14,
     lineHeight: 18,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type4: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 21,
     lineHeight: 25,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type5: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 15,
     lineHeight: 20,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type6: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 25,
     lineHeight: 30,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type7: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 14,
     lineHeight: 18,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type8: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type9: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type10: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type11: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type12: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type13: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 16,
-    color: masterColor.fontRed50
+    color: Color.fontRed50
   },
   type14: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontRed50
+    color: Color.fontRed50
   },
   type15: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 21,
     lineHeight: 25,
-    color: masterColor.fontRed50
+    color: Color.fontRed50
   },
   type16: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type17: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type18: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type19: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type20: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type21: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type22: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type23: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type24: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 13,
     lineHeight: 16,
     letterSpacing: 0.2,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type25: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type26: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type27: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 18,
     lineHeight: 22,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type28: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type29: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type30: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type31: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack40
+    color: Color.fontBlack40
   },
   type32: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 16,
     letterSpacing: 0.1,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type33: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 13,
     lineHeight: 16,
     letterSpacing: 0.2,
-    color: masterColor.fontBlack40
+    color: Color.fontBlack40
   },
   type34: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type35: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 15,
     lineHeight: 20,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type36: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontRed50
+    color: Color.fontRed50
   },
   type37: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type38: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type39: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type40: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type41: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type42: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type43: {
     fontFamily: Fonts.MontserratExtraBold,
     fontSize: 14,
     lineHeight: 18,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type44: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 9,
     lineHeight: 14,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type45: {
     fontFamily: Fonts.MontserratExtraBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type46: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type47: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type48: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type49: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type50: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type51: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontGreen50
+    color: Color.fontGreen50
   },
   type52: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontYellow10
+    color: Color.fontYellow10
   },
   type53: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type54: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 12,
     lineHeight: 16,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type55: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 15,
     lineHeight: 30,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type56: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type57: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type58: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontGreen50
+    color: Color.fontGreen50
   },
   type59: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type60: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type61: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 14,
     letterSpacing: 0.11,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type62: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type63: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type64: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 14,
     lineHeight: 18,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type65: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 9,
     lineHeight: 11,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type66: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 20,
     lineHeight: 24,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type67: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type68: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type69: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontYellow50
+    color: Color.fontYellow50
   },
   type70: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type71: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 10,
     lineHeight: 16,
     letterSpacing: 0.12,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type72: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 32,
     lineHeight: 41,
     letterSpacing: 3.2,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type73: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 9,
     lineHeight: 11,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type74: {
     fontFamily: Fonts.MontserratRegular,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontYellow50
+    color: Color.fontYellow50
   },
   type75: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 11,
     lineHeight: 16,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type76: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 11,
     lineHeight: 14,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type77: {
     fontFamily: Fonts.MontserratExtraBold,
     fontSize: 45,
     lineHeight: 55,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type78: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 16,
     lineHeight: 19,
-    color: masterColor.fontBlack50
+    color: Color.fontBlack50
   },
   type79: {
     fontFamily: Fonts.MontserratBlack,
     fontSize: 14,
     lineHeight: 18,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type80: {
     fontFamily: Fonts.MontserratExtraBold,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type81: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 19,
     lineHeight: 23,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type82: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 15,
     lineHeight: 25,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type83: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type84: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 13,
     lineHeight: 16,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type85: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack40
+    color: Color.fontBlack40
   },
   type86: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 15,
     lineHeight: 19,
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   type87: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 9,
     lineHeight: 11,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type88: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 12,
     lineHeight: 15,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type89: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 14,
     lineHeight: 18,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type90: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 8,
     lineHeight: 10,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type91: {
     fontFamily: Fonts.MontserratItalic,
     fontSize: 9,
     lineHeight: 11,
-    color: masterColor.fontBlack80
+    color: Color.fontBlack80
   },
   type92: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontGreen50
+    color: Color.fontGreen50
   },
   type93: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 14,
     lineHeight: 18,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type94: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 16,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type95: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 16,
-    color: masterColor.fontGreen50
+    color: Color.fontGreen50
   },
   type96: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 11,
     lineHeight: 16,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type97: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 9,
     lineHeight: 11,
-    color: masterColor.fontBlack60
+    color: Color.fontBlack60
   },
   type98: {
     fontFamily: Fonts.MontserratBold,
     fontSize: 11,
     lineHeight: 16,
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   type99: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontGreen50
+    color: Color.fontGreen50
   },
   forCompare: {
     fontFamily: Fonts.MontserratMedium,
     fontSize: 10,
     lineHeight: 13,
-    color: masterColor.fontGreen50
+    color: Color.fontGreen50
   },
   /** === bottom nav ==== */
   bottomNav: {
@@ -618,31 +618,31 @@ const styles = StyleSheet.create({
   /** === button text === */
   textButtonRedActive: {
     fontFamily: Fonts.MontserratSemiBold,
-    fontSize: 14,
+    fontSize: RFPercentage(2.1), //13
     lineHeight: 18,
     textAlign: 'center',
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   textButtonRedDisabled: {
     fontFamily: Fonts.MontserratSemiBold,
-    fontSize: 14,
+    fontSize: RFPercentage(2.1), //13,
     lineHeight: 18,
     textAlign: 'center',
-    color: masterColor.buttonActiveColorWhite
+    color: Color.buttonActiveColorWhite
   },
   textButtonWhiteActive: {
     fontFamily: Fonts.MontserratSemiBold,
-    fontSize: 14,
+    fontSize: RFPercentage(2.1), //13,
     lineHeight: 18,
     textAlign: 'center',
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   textButtonWhiteDisabled: {
     fontFamily: Fonts.MontserratSemiBold,
-    fontSize: 14,
+    fontSize: RFPercentage(2.1), //13,
     lineHeight: 18,
     textAlign: 'center',
-    color: masterColor.buttonRedDisableColor
+    color: Color.buttonRedDisableColor
   },
   /** === button small text === */
   textButtonSmallRedActive: {
@@ -650,28 +650,28 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 15,
     textAlign: 'center',
-    color: masterColor.fontWhite
+    color: Color.fontWhite
   },
   textButtonSmallRedDisabled: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 15,
     textAlign: 'center',
-    color: masterColor.buttonActiveColorWhite
+    color: Color.buttonActiveColorWhite
   },
   textButtonSmallWhiteActive: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 15,
     textAlign: 'center',
-    color: masterColor.mainColor
+    color: Color.mainColor
   },
   textButtonSmallWhiteDisabled: {
     fontFamily: Fonts.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 15,
     textAlign: 'center',
-    color: masterColor.buttonRedDisableColor
+    color: Color.buttonRedDisableColor
   }
 });
 
