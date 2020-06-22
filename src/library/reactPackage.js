3e9b51dcf4f958f8713709edbce4b2e8d96e89f5
hasapu <hari.sakti.putra@gmail.com>
2020-06-22 11:46:50 +0700

    CORE LIBRARY -> add library for import react package, thirparty package and component
    Former-commit-id: da2fe52221fccf0942c8027e303f2ca31b85f188

---
+++ src/library/reactPackage.js
@@ -0,0 +1,43 @@
+/**
+ * THIS LIBRARY FOR REACT PACKAGE
+ */
+import React, { Component } from 'react';
+import {
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  SafeAreaView,
+  RefreshControl,
+  Dimensions,
+  ScrollView,
+  TextInput,
+  Keyboard,
+  Image,
+  FlatList,
+  BackHandler
+} from 'react-native';
+import Text from 'react-native-text';
+/** FOR DIMENSIONS */
+const { width, height } = Dimensions.get('window');
+
+export {
+  React,
+  Component,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  SafeAreaView,
+  RefreshControl,
+  Dimensions,
+  ScrollView,
+  Keyboard,
+  width,
+  height,
+  Image,
+  FlatList,
+  BackHandler
+};
---
+++ src/library/thirdPartyPackage.js
@@ -0,0 +1,53 @@
+/**
+ * THIS LIBRARY FOR THIRD PARTY PACKAGE
+ */
+/** REDUX */
+import { bindActionCreators } from 'redux';
+import { connect, Provider } from 'react-redux';
+/** REDUX PERSIST */
+import { PersistGate } from 'redux-persist/integration/react';
+/** ICON */
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import OcticonsIcon from 'react-native-vector-icons/Octicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+/** MAPS */
+import MapView, { Marker } from 'react-native-maps';
+/** GEOLOCATION */
+import Geolocation from 'react-native-geolocation-service';
+/** NAVIGATION */
+import { NavigationEvents } from 'react-navigation';
+/** OPEN SETTING */
+import OpenAppSettings from 'react-native-app-settings';
+/** CAROUSEL */
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+/** SKELETON */
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+
+export {
+  /** REDUX */
+  bindActionCreators,
+  connect,
+  Provider,
+  /** REDUX PERSIST */
+  PersistGate,
+  /** ICON */
+  MaterialIcon,
+  MaterialCommunityIcons,
+  OcticonsIcon,
+  Ionicons,
+  /** MAPS */
+  MapView,
+  Marker,
+  /** GEOLOCATION */
+  Geolocation,
+  /** NAVIGATION */
+  NavigationEvents,
+  /** OPEN SETTING */
+  OpenAppSettings,
+  /** CAROUSEL */
+  Carousel,
+  Pagination,
+  /** SKELETON */
+  SkeletonPlaceholder
+};
