4b3246cc60fb94276bde45c61018518839df5b69
hasapu <hari.sakti.putra@gmail.com>
2020-06-22 15:25:07 +0700

    CORE LIBRARY -> add component library
    Former-commit-id: 5855c5fb796f4467bfb1094e893f357d627097aa

---
+++ src/library/component.js
@@ -0,0 +1,86 @@
+import CartGlobal from '../components/CartGlobal';
+/** SEARCH BAR */
+import SearchBarType1 from '../components/search_bar/SearchBarType1';
+import SearchBarType3 from '../components/search_bar/SearchBarType3';
+/** BACK HANDLER */
+import BackHandlerCloseApp from '../components/BackHandlerCloseApp';
+import BackHandlerBackSpecific from '../components/BackHandlerBackSpecific';
+/** STATUS BAR */
+import {
+  StatusBarRed,
+  StatusBarWhite,
+  StatusBarRedOP50,
+  StatusBarBlackOP40
+} from '../components/StatusBarGlobal';
+/** INPUT */
+import InputType2 from '../components/input/InputType2';
+/** INPUT MAPS (PIN POINT) */
+import InputMapsType1 from '../components/input/InputMapsType1';
+/** DROPDOWN */
+import DropdownType1 from '../components/input/DropdownType1';
+/** BUTTON */
+import ButtonSingle from '../components/button/ButtonSingle';
+import ButtonMenuType1 from '../components/button/ButtonMenuType1';
+/** MODAL */
+import ModalConfirmation from '../components/modal/ModalConfirmation';
+import ModalBottomErrorPinMap from '../components/error/ModalBottomErrorPinMap';
+import ModalBottomSwipeCloseNotScroll from '../components/modal_bottom/ModalBottomSwipeCloseNotScroll';
+import ModalBottomType1 from '../components/modal_bottom/ModalBottomType1';
+import ModalBottomType3 from '../components/modal_bottom/ModalBottomType3';
+/** ERROR MODAL */
+import ModalBottomErrorRespons from '../components/error/ModalBottomErrorRespons';
+/** OTP */
+import OtpInput from '../components/otp/OtpInput';
+import OtpResend from '../components/otp/OtpResend';
+/** PROGRESS BAR */
+import ProgressBarType1 from '../components/progress_bar/ProgressBarType1';
+/** LOADING */
+import { LoadingPage, LoadingLoadMore } from '../components/Loading';
+/** ERROR PAGE */
+import ErrorPageNoGPS from '../components/error/ErrorPageNoGPS';
+/** TOAST */
+import ToastType1 from '../components/toast/ToastType1';
+
+export {
+  CartGlobal,
+  /** SEARCH BAR */
+  SearchBarType1,
+  SearchBarType3,
+  /** BACK HANDLER */
+  BackHandlerCloseApp,
+  BackHandlerBackSpecific,
+  /** STATUS BAR */
+  StatusBarRed,
+  StatusBarWhite,
+  StatusBarRedOP50,
+  StatusBarBlackOP40,
+  /** INPUT */
+  InputType2,
+  /** INPUT MAPS (PIN POINT) */
+  InputMapsType1,
+  /** DROPDOWN */
+  DropdownType1,
+  /** BUTTON */
+  ButtonSingle,
+  ButtonMenuType1,
+  /** MODAL */
+  ModalConfirmation,
+  ModalBottomErrorPinMap,
+  ModalBottomSwipeCloseNotScroll,
+  ModalBottomType1,
+  ModalBottomType3,
+  /** ERROR MODAL */
+  ModalBottomErrorRespons,
+  /** OTP */
+  OtpInput,
+  OtpResend,
+  /** PROGRESS BAR */
+  ProgressBarType1,
+  /** LOADING */
+  LoadingPage,
+  LoadingLoadMore,
+  /** ERROR PAGE */
+  ErrorPageNoGPS,
+  /** TOAST */
+  ToastType1
+};
