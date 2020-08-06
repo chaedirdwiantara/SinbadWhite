091021694d4ed6df08a2abcb98c9bbba569d6573
Tatas Fachrul <tatasfachrul@gmail.com>
2020-08-06 15:31:35 +0700

    PROFILE AREA MAPPING -> Remove unused state
    Former-commit-id: 5987910ef04773957ff228e3f387f64ec68ada24

--- src/screens/profiles/ProfileAreaMapping.js
+++ src/screens/profiles/ProfileAreaMapping.js
@@ -10,44 +10,30 @@ import {
 import { MaterialIcon, connect, bindActionCreators } from '../../library/thirdPartyPackage';
 import { StatusBarWhite, CardType1, BackHandlerBackSpecific, LoadingPage } from '../../library/component';
 import { Color } from '../../config';
-import NavigationService from '../../navigation/NavigationService';
+import NavigationService from '../../navigation/NavigationService'; 
 import * as ActionCreators from '../../state/actions'
 
 class ProfileAreaMapping extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data : [
-        {
-          warehouse: 'DC Serang'
-        },
-        {
-          warehouse: 'DC Balaraja'
-        },
-        {
-          warehouse: 'DC Rangkas Bitung'
-        }        
-      ],
-      // navigateBack: this.props.global.pageAddMerchantFrom
-    };
   }
 
-  static navigationOptions = ({ navigation }) => {
-    return {
-      headerLeft: () => (
-        <TouchableOpacity
-          style={{ marginLeft: 16 }}
-          onPress={() => NavigationService.navigate(this.props.global.pageAddMerchantFrom)}
-        >
-          <MaterialIcon
-            color={Color.fontBlack50}
-            name={'arrow-back'}
-            size={24}
-          />
-        </TouchableOpacity>
-      )
-    };
-  };
+  // static navigationOptions = ({ navigation }) => {
+  //   return {
+  //     headerLeft: () => (
+  //       <TouchableOpacity
+  //         style={{ marginLeft: 16 }}
+  //         onPress={() => NavigationService.navigate(this.props.global.pageAddMerchantFrom)}
+  //       >
+  //         <MaterialIcon
+  //           color={Color.fontBlack50}
+  //           name={'arrow-back'}
+  //           size={24}
+  //         />
+  //       </TouchableOpacity>
+  //     )
+  //   };
+  // };
 
   componentDidMount(){
     this.props.profileGetWarehouseProcess()
