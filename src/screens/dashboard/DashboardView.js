3f7a42b72c1ccc782ed69dfa195e179e360faf4f
bismark <bismark.john.anes@gmail.com>
2020-08-14 08:46:09 +0700

    dashboard detail graphic stable
    Former-commit-id: 0c945d4493f52da6b7034befe2720d93de71636f

--- src/screens/dashboard/DashboardView.js
+++ src/screens/dashboard/DashboardView.js
@@ -487,45 +487,12 @@ class DashboardView extends Component {
                     data: item.data.data[0].names.data,
                     axisLabel: {
                       rotate: 30,
-                      formatter: (value) => {
-                        var match = value.match(/-([0-9]{1,2}$)/);
-
-                        if (match && match.length === 2) return match[1];
-
-                        return value;
-                      }
                     },
                   },
                   yAxis: {
                     type: 'value',
                     axisLabel: {
                       rotate: 30,
-                      formatter: value => {
-                        /*
-                         * TODO: inject this js into webview
-                         */
-                        const jsNumberFormat = (num, digits) => {
-                          var si = [
-                            { value: 1, symbol: '' },
-                            { value: 1E3, symbol: 'Rb' },
-                            { value: 1E6, symbol: 'Jt' },
-                            { value: 1E9, symbol: 'M' },
-                            { value: 1E12, symbol: 'T' },
-                            { value: 1E15, symbol: 'P' },
-                            { value: 1E18, symbol: 'E' }
-                          ];
-                          var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-                          var i;
-                          for (i = si.length - 1; i > 0; i--) {
-                            if (num >= si[i].value) {
-                              break;
-                            }
-                          }
-                          return (num / si[i].value).toFixed(digits).replace(rx, '$1') + si[i].symbol;
-                        };
-
-                        return jsNumberFormat(value, 1);
-                      }
                     }
                   },
                   series: item.data.data[0].series.map((seri) => {
@@ -538,7 +505,7 @@ class DashboardView extends Component {
                       name: seri.name
                     };
                   })
-                }
+                };
 
                 chartOption.legend = {
                   data: legend
