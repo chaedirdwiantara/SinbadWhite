463d756c49e0c83c3b8e39d36f675d1e93aaa88b
bondhan bondhan <bondhan@sinbad.co.id>
2020-09-05 09:54:11 +0700

    adding apiHost to .gitignore
    Former-commit-id: d2f9babd85559453d517e1c90c7054c0f14a3a2f

--- src/services/apiHost.js
+++ src/services/apiHost.js
@@ -1,8 +1,5 @@
 const ApiHost = {
-  url: 'https://kong-dev.sinbad.web.id/',
-  urlDev: 'https://kong-dev.sinbad.web.id/',
-  urlStg: 'https://kong-stg.sinbad.web.id/',
-  urlProd: 'https://kong.sinbad.web.id/',
+  url: 'https://kong.sinbad.web.id/',
   urlMap:
     'https://maps.googleapis.com/maps/api/geocode/json?result_type=street_address&key=AIzaSyDFMdAZxE_n7KoDxroBb61MAuxki4mTI4Q'
 };
