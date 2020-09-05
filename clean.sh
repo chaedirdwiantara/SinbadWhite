1af3e35c9747d80a47b0aaf1ddd839c8f0d0c563
bondhan bondhan <bondhan@sinbad.co.id>
2020-09-05 09:56:26 +0700

    update clean.sh
    Former-commit-id: 021f9c164467892d0f47abda39d411b9ebb073c3

--- clean.sh
+++ clean.sh
@@ -5,3 +5,4 @@ rm -rf ./android
 rm -rf ./ios
 rm index.js
 rm package.json
+rm src/services/apiHost.js
--- src/services/apiHost.js
+++
@@ -1,7 +0,0 @@
-const ApiHost = {
-  url: 'https://kong.sinbad.web.id/',
-  urlMap:
-    'https://maps.googleapis.com/maps/api/geocode/json?result_type=street_address&key=AIzaSyDFMdAZxE_n7KoDxroBb61MAuxki4mTI4Q'
-};
-
-export default ApiHost;
