1d46e5d3cc5869236cb7071ee9caf0737b15efac
bondhan bondhan <bondhan@sinbad.co.id>
2020-09-10 15:48:52 +0700

    update .gilab-ci.yml and reformat
    Former-commit-id: ca1437d27b62c5f19c8f1a73237d5464a08c4030

--- build.sh
+++ build.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
+if [ ! -z "$1" ]; then
 
-if [ ! -z "$1" ]
-then
+  if [[ $1 != "development" && $1 != "staging" && $1 != "master" ]]; then
 
-	if [[ $1 != "development" && $1 != "staging" && $1 != "master"  ]];
-	then
+    echo "usage: build.sh [development | staging | master] "
 
-		echo "usage: build.sh [development | staging | master] "
+  else
 
-	else
+    echo "building for" $1
+    rm -rf ./android
+    rm -rf ./ios
+    rm -rf ./index.js
+    rm -rf ./package.json
+    cp -rf ./environment/$1/android_$1 ./android
+    cp -rf ./environment/$1/ios_$1 ./ios
+    cp -f ./environment/$1/index_$1.js ./index.js
+    cp -f ./environment/$1/package_$1.json ./package.json
+    cp -f ./environment/$1/apiHost_$1.js ./src/services/apiHost.js
 
-		echo "building for" $1
-		rm -rf ./android ; rm -rf ./ios ; rm -rf ./index.js ; rm -rf ./package.json
-		cp -rf ./environment/$1/android_$1 ./android
-		cp -rf ./environment/$1/ios_$1 ./ios
-		cp -f ./environment/$1/index_$1.js ./index.js
-		cp -f ./environment/$1/package_$1.json ./package.json
-		cp -f ./environment/$1/apiHost_$1.js ./src/services/apiHost.js
+    rm -rf node_modules
+    npm install
+    npm add react-native-firebase
+    yarn add @react-native-community/toolbar-android
+    cd android && chmod +x gradlew && rm -rf ./.gradle && ./gradlew clean && ./gradlew cleanBuildCache && ./gradlew app:assembleRelease
+    # npm run build
 
-		rm -rf node_modules
-	    	npm install
-	    	npm add react-native-firebase
-	   	yarn add @react-native-community/toolbar-android
-	    	cd android && chmod +x gradlew && rm -rf ./.gradle && ./gradlew clean && ./gradlew cleanBuildCache && ./gradlew app:assembleRelease
-	    	# npm run build
-
-	fi
+  fi
 
 else
-	echo "usage: build.sh [development | staging | master] "
+  echo "usage: build.sh [development | staging | master] "
 fi
