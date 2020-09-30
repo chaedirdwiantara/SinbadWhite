9a472538290e0793b0e24222664e393b1814be19
Tatas Fachrul <tatas@sinbad.co.id>
2020-09-30 11:10:55 +0700

    EC2-253: COMPONENTS -> Refactor Accordion List
    Former-commit-id: 90872df4511c274851813bcbb94ffe598a1eff74

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -5,66 +5,114 @@ import {
   TouchableOpacity,
   Text,
   StyleSheet,
-  LayoutAnimation,
-  Platform
+  LayoutAnimation
 } from '../../library/reactPackage'
 import {
   MaterialIcon
 } from '../../library/thirdPartyPackage'
 import { Color } from '../../config'
+import { Fonts } from '../../helpers'
 
 class Accordion extends Component{
   constructor(props){
     super(props)
     this.state = {
-      data: props.data,
-      expanded: false
+      promoId: null
     }
   }
 
-  render(){
+  toggleExpand(promoId){
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+    this.setState({expanded: !this.state.expanded, promoId: promoId})
+  }
+
+  renderItem({ item, index }){
     return(
-      <View>
-        <TouchableOpacity ref={this.accordion} style={styles.row}>
-          <Text style={styles.title}>Title</Text>
-          <MaterialIcon name={this.state.expanded ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={30} />
+      <View key={index} >
+        <TouchableOpacity ref={this.accordion} style={styles.row} onPress={() => this.toggleExpand(item.id)}>
+          <Text style={Fonts.type8}>{item.title}</Text>
+          <MaterialIcon name={item.id === this.state.promoId ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={30} />
         </TouchableOpacity>
+        <View style={{ backgroundColor: Color.fontBlack10, height: 1, marginTop: -10}} />
         <View style={styles.parentHr}>
           {
-            this.state.expanded && 
+            item.id === this.state.promoId &&
             <View style={styles.child}> 
-              <Text>This is the Content</Text>
+              <Text style={[Fonts.type8, { marginHorizontal: 16, marginTop: 10 }]}>{item.description}</Text>
             </View>
           }
         </View>
       </View>
     )
   }
+
+  renderAccordion(){
+    return(
+      <View style={{marginBottom: 16}}>
+        <TouchableOpacity ref={this.accordion} style={styles.row} onPress={() => this.toggleExpand()}>
+          <Text style={[Fonts.type8]}>{this.props.data[0].title}</Text>
+          <MaterialIcon name={this.state.expanded ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={30} />
+        </TouchableOpacity>
+        <View style={{ backgroundColor: Color.fontBlack10, height: 1, marginTop: -10}} />
+        <View style={styles.parentHr}>
+          {
+            this.state.expanded && 
+            <View style={styles.child}> 
+              <Text style={Fonts.type8}>{this.props.data[0].description}</Text>
+            </View>
+          }
+        </View>
+      </View>
+    )
+  }
+
+  renderData(){
+    return(
+      <View>
+        {
+          this.props.data.map((item, index) => (
+            this.renderItem({item, index})
+          ))
+        }
+      </View>
+    )
+  }
+
+  render(){
+    return(
+      <View style={styles.mainContainer}>
+      {this.renderData()}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  title:{
-      fontSize: 14,
-      fontWeight:'bold',
-      color: Color.backgroundWhite,
+  mainContainer: {
+    flex: 1,
+    backgroundColor: Color.backgroundWhite
   },
   row:{
       flexDirection: 'row',
       justifyContent:'space-between',
       height:56,
-      paddingLeft:25,
+      paddingLeft:16,
       paddingRight:18,
       alignItems:'center',
-      backgroundColor: Color.backgroundWhite
+      backgroundColor: Color.backgroundWhite,
+      flex: 1
   },
   parentHr:{
       height:1,
       color: Color.backgroundWhite,
-      width:'100%'
+      width:'100%',
+      height: '100%',
+      flex: 1
   },
   child:{
       backgroundColor: Color.backgroundWhite,
-      padding:16,
+      height: '100%',
+      flex: 1
   }
   
 });
