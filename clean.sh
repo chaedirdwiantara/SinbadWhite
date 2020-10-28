056c09cbdd2c969227b8ae6b223721ed44cfa23c
Muhammad Hafidz Istihori <me@hafidz.id>
2020-10-28 17:39:34 +0700

    remove unused file or directory

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "clean up"
-rm -rf ./android
-rm -rf ./ios
-rm index.js
-rm package.json
-rm src/services/apiHost.js
--- environment/development/index_development.js
+++
@@ -1,8 +0,0 @@
-/**
- * @format
- */
-import { AppRegistry } from './src/library/reactPackage';
-import Main from './Main';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => Main);
