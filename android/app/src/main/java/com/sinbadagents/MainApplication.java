e4d78a5760cd1d76770219cae4b6ecb80e7472f1
Muhammad Hafidz Istihori <me@hafidz.id>
2020-10-28 17:21:46 +0700

    add code push library

--- android/app/src/main/java/com/sinbadagents/MainApplication.java
+++ android/app/src/main/java/com/sinbadagents/MainApplication.java
@@ -14,6 +14,7 @@ import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,6 +23,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+          return CodePush.getJSBundleFile();
         }
 
         @Override
