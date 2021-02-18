f70a66c4fd9eab7b20ac8fe53e12b390a5223492
ayu-khairina <ayu@sinbad.co.is>
2021-02-18 17:40:46 +0700

    SFA - add collection type tag

--- src/screens/sfa/SfaView.js
+++ src/screens/sfa/SfaView.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   SafeAreaView,
-  TouchableOpacity
+  TouchableOpacity,
+  Image
 } from '../../library/reactPackage';
 
 import {
@@ -15,6 +16,9 @@ import {
 } from '../../library/thirdPartyPackage';
 import {
   LoadingPage,
+  StatusBarWhite,
+  SearchBarType1,
+  TagListType2
 } from '../../library/component';
 import { Fonts, GlobalStyle, MoneyFormat } from '../../helpers';
 import masterColor from '../../config/masterColor.json';
@@ -25,37 +29,75 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function SfaView(props) {
   const dispatch = useDispatch();
+  const [searchText, setSearchText] = useState('');
+  const [sfaTag, setSfaTag] = useState([
+    { status: '', title: 'Semua', detail: '' },
+    {
+      status: 'waiting_for_payment',
+      title: 'Menunggu Pembayaran',
+      detail: 'Sedang Menunggu Pembayaran'
+    },
+    { status: 'overdue', title: 'Overdue', detail: 'Pesanan Sudah Overdue' }
+  ]);
+  const [selectedTagStatus, setSelectedTagStatus] = useState('semua');
+  const [data, setData] = useState({
+    data: [
+      {
+        orderCode: 'S0100042273101076686',
+        orderRef: 'SNB1812/0002134',
+        total: 670000,
+        debtDate: '25-02-2021',
+        overdue: '27-02-2021',
+        paidAmount: 0
+      },
+      {
+        orderCode: 'S0213042273101076686',
+        orderRef: 'SNB1815/0002135',
+        total: 700000,
+        debtDate: '26-02-2021',
+        overdue: '28-02-2021',
+        paidAmount: 0
+      },
+    ],
+    totalInvoice: 3,
+    invoiceAmount: 1625000,
+    totalAmountPaid: 335000,
+    outstanding: 1675000
+  });
 
   /**
    * =======================
    * FUNCTIONAL
    * =======================
    */
-  
 
   /**
    * *********************************
    * RENDER VIEW
    * *********************************
    */
-  const renderHeader = () => {
-    return (
-        <View>
-            <TouchableOpacity onPress={() => NavigationService.navigate('SfaDetailView')}>
-                <Text>Header</Text>
-            </TouchableOpacity>
-        </View>
-    )
-  }
 
   const renderBody = () => {
     return (
-        <View>
-            <Text>Body</Text>
-        </View>
-    )
-  }
-
+      <View>
+        <Text>Body</Text>
+      </View>
+    );
+  };
+  /** === TAGS SECTION === */
+  const renderTagsContent = () => {
+    return (
+      <View>
+        <TagListType2
+          selected={selectedTagStatus}
+          // onRef={ref => (this.parentFunction = ref)}
+          // parentFunction={this.parentFunction.bind(this)}
+          data={sfaTag}
+        />
+        <View style={GlobalStyle.lines} />
+      </View>
+    );
+  };
   /**
    * ==================================
    * RENDER CONTENT DATA (MAIN VIEW)
@@ -64,13 +106,50 @@ function SfaView(props) {
   const renderContent = () => {
     return (
       <View style={{ flex: 1 }}>
-        {renderHeader()}
+        {renderSearchAndFilter()}
+        {renderTagsContent()}
         {renderBody()}
       </View>
     );
   };
 
-  
+  /** === RENDER FILTER AND SEARCH === */
+  const renderSearchAndFilter = () => {
+    return (
+      <View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 1 }}>
+            <SearchBarType1
+              placeholder={'Cari produk, nomor pesanan'}
+              searchText={searchText}
+              // onRef={ref => (this.parentFunction = ref)}
+              // parentFunction={this.parentFunction.bind(this)}
+            />
+          </View>
+          <TouchableOpacity
+            style={{ paddingRight: 16, justifyContent: 'center' }}
+            onPress={() => console.log('clicked')}
+          >
+            {/* {this.state.portfolioId.length > 0 ||
+            this.state.dateFilter.dateGte !== '' ||
+            this.state.dateFilter.dateLte !== '' ? (
+              <View style={styles.circleFilter} />
+            ) : (
+              <View />
+            )
+             } */}
+
+            <Image
+              source={require('../../assets/icons/pdp/filter.png')}
+              style={{ height: 24, width: 24 }}
+            />
+            <Text style={Fonts.type67}>Filter</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={GlobalStyle.lines} />
+      </View>
+    );
+  };
 
   /**
    * =======================
@@ -80,11 +159,10 @@ function SfaView(props) {
   return (
     <>
       {/* {props.merchant.dataGetMerchantDetail ? ( */}
-        <SafeAreaView style={styles.mainContainer}>
-          <View style={{ flex: 1 }}>
-            {renderContent()}
-          </View>
-        </SafeAreaView>
+      <SafeAreaView style={styles.mainContainer}>
+        <StatusBarWhite />
+        <View style={{ flex: 1 }}>{renderContent()}</View>
+      </SafeAreaView>
       {/* ) : (
         <LoadingPage />
       )} */}
@@ -95,7 +173,7 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     backgroundColor: masterColor.backgroundWhite
-  },
+  }
 });
 
 const mapStateToProps = ({ user, merchant }) => {
