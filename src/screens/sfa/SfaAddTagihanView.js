30753c9479a9356e6d318de86b9a2fa5fbaecd2c
tyo-sinbad <prasetyo@sinbad.co.id>
2021-02-19 10:50:21 +0700

    add metode penagihan

--- src/screens/sfa/SfaAddTagihanView.js
+++ src/screens/sfa/SfaAddTagihanView.js
@@ -27,6 +27,20 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function SfaAddTagihanView(props) {
   const dispatch = useDispatch();
+  const [data, setData] = useState(
+    {
+      "data": {
+        "id": 1,
+        "orderCode": "S010004232321231231",
+        "orderRef": "A754123131",
+        "invoiceGroupName": "COMBINE",
+        "totalBilling": 670000,
+        "totalInstorePayment": 335000,
+        "totalCollection": 0,
+        "remainingBilling": 335000,
+      }
+    }
+  );
 
   /**
    * =======================
@@ -48,25 +62,43 @@ function SfaAddTagihanView(props) {
               <Text style={Fonts.type48}>Informasi Faktur</Text>
             </View>
             <View style={[GlobalStyle.lines, { flex: 1, marginVertical: 8 }]} />
-            {/* {renderItemFakturInfo()} */}
+            {renderItemFakturInfo()}
           </View>
         </View>
     )
   }
 
+  const renderItemFakturInfo = () => {
+    return (
+      <View>
+        <View style={{flexDirection:"row", marginBottom: 8, justifyContent: "space-between"}}>
+          <Text style={Fonts.type17}>Nama Faktur</Text>
+          <Text style={Fonts.type17}>{data.data.invoiceGroupName}</Text>
+        </View>
+        <View style={{flexDirection:"row", marginBottom: 8, justifyContent: "space-between"}}>
+          <Text style={Fonts.type17}>No. Pesanan</Text>
+          <Text style={Fonts.type17}>{data.data.orderCode}</Text>
+        </View>
+        <View style={{flexDirection:"row", marginBottom: 8, justifyContent: "space-between"}}>
+          <Text style={Fonts.type17}>No. Referensi</Text>
+          <Text style={Fonts.type17}>{data.data.orderRef}</Text>
+        </View>
+      </View>
+    )
+  }
+
   const renderCollectionInfo = () => {
     return (
-        <View style={[styles.container, {marginBottom:16}]}>
+        <View style={styles.container}>
           <View style={[styles.cardTaskList, GlobalStyle.shadowForBox5]}>
             <View>
               <Text style={Fonts.type48}>Informasi Tagihan</Text>
             </View>
             <View style={[GlobalStyle.lines, { flex: 1, marginVertical: 8 }]} />
-            {/* {renderItemTagihanInfo()} */}
-            <View style={[GlobalStyle.lines, { flex: 1, marginVertical: 8 }]} />
-            {/* {renderTagihanDetail()} */}
-            <View style={[GlobalStyle.lines, { flex: 1, marginVertical: 8 }]} />
-            {/* {renderTagihanOutstanding()} */}
+            <View style={{flexDirection:"row", marginBottom: 8, justifyContent: "space-between"}}>
+              <Text style={Fonts.type17}>Sisa Tagihan</Text>
+              <Text style={Fonts.type22}>{MoneyFormat(data.data.remainingBilling)}</Text>
+            </View>
           </View>
         </View>
     )
@@ -74,13 +106,31 @@ function SfaAddTagihanView(props) {
 
   const renderCollectionDetail = () => {
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, {marginBottom:16}]}>
           <View style={[styles.cardTaskList, GlobalStyle.shadowForBox5]}>
             <View>
               <Text style={Fonts.type48}>Detail Penagihan</Text>
             </View>
-            <View style={[GlobalStyle.lines, { flex: 1, marginVertical: 8 }]} />
-            {/* {renderItemFakturInfo()} */}
+            <View style={[GlobalStyle.lines, { flex: 1, marginTop: 8, marginBottom: 16 }]} />
+            <View>
+              <Text style={Fonts.type17}>MetodePenagihan</Text>
+            </View>
+            <View>
+              <TouchableOpacity
+                style={styles.boxMenu}
+                onPress={() => alert("test")}
+              >
+                <Text style={[Fonts.type17, {opacity: 0.5}]}>Pilih Metode Penagihan</Text>
+                <View style={{ position: 'absolute', right: 16 }}>
+                  <MaterialIcon
+                    name="chevron-right"
+                    color={masterColor.fontBlack40}
+                    size={24}
+                  />
+                </View>
+              </TouchableOpacity>
+              <View style={[GlobalStyle.lines]} />
+            </View>
           </View>
         </View>
     )
@@ -137,6 +187,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     borderRadius: 4,
     backgroundColor: masterColor.backgroundWhite
+  },
+  boxMenu: {
+    // paddingHorizontal: 16,
+    paddingVertical: 17,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
 });
 
