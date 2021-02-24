bb5800270241e0257dbd1e3325333eb7e517f284
Tatas Fachrul <tatas@sinbad.co.id>
2021-02-24 12:22:15 +0700

    OMS-167 MERCHANT HOME > Disable to catatan stock when agent not checkin

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,7 +16,7 @@ export const TOTAL_PESANAN = 'total.pesanan';
  */
 export const ACTIVITY_JOURNEY_PLAN_CHECK_IN = 'check_in';
 export const ACTIVITY_JOURNEY_PLAN_ORDER = 'order';
-export const ACTIVITY_JOURNEY_PLAN_STOCK = 'stock';
+export const ACTIVITY_JOURNEY_PLAN_STOCK = 'record_stock';
 export const ACTIVITY_JOURNEY_PLAN_CHECK_OUT = 'check_out';
 
 /*
