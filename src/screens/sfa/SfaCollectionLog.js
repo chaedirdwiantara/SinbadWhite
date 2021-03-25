e26a6f6bf0163f098037937f8a38c04afcddc596
tyo-sinbad <prasetyo@sinbad.co.id>
2021-03-25 11:08:15 +0700

    SFA -> add icon and navigate to transaction log

---
+++ src/screens/sfa/SfaCollectionLog.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+} from '../../library/reactPackage';
+
+import {
+  bindActionCreators,
+  connect,
+  MaterialIcon
+} from '../../library/thirdPartyPackage';
+import NavigationService from '../../navigation/NavigationService';
+import * as ActionCreators from '../../state/actions';
+
+function SfaCollectionLog(props) {
+
+    /**
+   * =======================
+   * MAIN
+   * =======================
+   */
+  return (
+    <View>
+        <TouchableOpacity onPress={()=> alert("goTo added collection")}>
+            <Text>Go To Added collection</Text>
+        </TouchableOpacity>
+    </View>
+  );
+
+}
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators(ActionCreators, dispatch);
+  };
+  
+  export default connect(
+    mapDispatchToProps
+  )(SfaCollectionLog);
