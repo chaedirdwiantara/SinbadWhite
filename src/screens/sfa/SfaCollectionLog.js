a3a64961583052326702620133dfb3317b53cc2a
tyo-sinbad <prasetyo@sinbad.co.id>
2021-04-05 11:32:48 +0700

    SFA -> integrate collection detail cash

--- src/screens/sfa/SfaCollectionLog.js
+++ src/screens/sfa/SfaCollectionLog.js
@@ -81,9 +81,10 @@ function SfaCollectionLog(props) {
 
     //**RENDER ITEM */
     const renderItem = ({item, index}) => {
+        console.log("dataaa:", item);
         return(
             <View key={index}>
-                    <TouchableOpacity onPress={()=> NavigationService.navigate('SfaCollectionDetailView', {paymentCollectionId: item.paymentCollectionId})}>
+                    <TouchableOpacity onPress={()=> NavigationService.navigate('SfaCollectionDetailView', {paymentCollectionId: item.id})}>
                         <View style={{flexDirection: "row", justifyContent:"space-between", marginHorizontal:16, marginVertical: 16}}>
                             <View style={{flex: 2}}>
                                 <Text style={{...Fonts.type42, marginBottom: 8}}>{item.salesName}</Text>
