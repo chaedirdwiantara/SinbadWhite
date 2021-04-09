db2917f22cd7aa6871edbef25eec77ffb192fc3b
mazdha94 <muhammad.ali@sinbad.co.id>
2021-04-09 15:49:21 +0700

    Remove unused code

--- src/state/types/PrivilegeType.js
+++ src/state/types/PrivilegeType.js
@@ -7,5 +7,3 @@
 export const PRIVILEGE_GET_PROCESS = 'PrivilegeGetProcess'
 export const PRIVILEGE_GET_SUCCESS = 'PrivilegeGetSuccess'
 export const PRIVILEGE_GET_FAILED = 'PrivilegeGetFailed'
-export const REMAPPING_PRIVILEGE = 'RemappingPrivilege'
-export const PRIVILEGE_SET_SALES_ROLE = 'PrivilegeSetSalesRole'
