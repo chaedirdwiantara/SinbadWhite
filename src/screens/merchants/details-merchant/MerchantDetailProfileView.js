de20b395257632c68d3a4b9c2d0a38c137d27566
mazdha94 <muhammad.ali@sinbad.co.id>
2021-04-13 10:04:56 +0700

    => Add button to show maps route 	modified:   src/screens/merchants/MerchantDetailMapView.js => Add permission location 	modified:   src/screens/merchants/MerchantDetailView.js => Add badge terverifikasi 	modified:   src/screens/merchants/details-merchant/MerchantDetailProfileView.js 	modified:   src/state/reducers/MerchantReducer.js

--- src/screens/merchants/details-merchant/MerchantDetailProfileView.js
+++ src/screens/merchants/details-merchant/MerchantDetailProfileView.js
@@ -8,7 +8,8 @@ import {
 } from '../../../library/reactPackage';
 import {
   bindActionCreators,
-  connect
+  connect,
+  MaterialCommunityIcons
 } from '../../../library/thirdPartyPackage';
 import { Fonts } from '../../../helpers';
 import { Color } from '../../../config';
@@ -51,71 +52,85 @@ class MerchantDetailProfileView extends Component {
     return (
       <View style={styles.boxContent}>
         <View>
-          <Text style={[Fonts.type9, { marginBottom: 6 }]}>{data.key}</Text>
+          {this.renderLabel(data)}
           <Text style={data.fontType}>{this.checkRejectionValue(data)}</Text>
         </View>
       </View>
     );
   }
+  renderLabel(data){
+    if(data.verified){
+        return (
+          <View style={{flexDirection: 'row', marginBottom: 6}}>
+            <Text style={Fonts.type9}>{data.key}</Text>
+            <View style={styles.emailBadge}>
+              <MaterialCommunityIcons 
+                name="check" 
+                color={Color.fontWhite}
+                size={9} 
+                style={{marginHorizontal: 4}} />
+              <Text 
+                style={[Fonts.type9, {fontSize: 9, color: Color.fontWhite}]}>
+                  Terverifikasi
+              </Text>
+            </View>
+          </View>
+        )
+      } else {
+        return <Text style={[Fonts.type9, { marginBottom: 6 }]}>{data.key}</Text>
+      }
+  }
   /** === RENDER CONTENT === */
   renderContent() {
+    const {dataMerchantVolatile} = this.props.merchant
     return (
       <View>
         {this.renderContentSection({
           key: 'Nama Lengkap Pemilik',
           failed: this.checkRejectionStatus('fullName'),
-          fontType: this.checkRejectionStatus('fullName')
-            ? Fonts.type84
-            : Fonts.type24,
-          value: this.props.merchant.dataMerchantVolatile.fullName
+          fontType: this.checkRejectionStatus('fullName') ? Fonts.type84 : Fonts.type24,
+          value: dataMerchantVolatile.fullName
         })}
         {this.renderContentSection({
           key: 'Nomor Handphone',
           fontType: Fonts.type24,
-          value: this.props.merchant.dataMerchantVolatile.phone
+          value: dataMerchantVolatile.phone,
+          verified: dataMerchantVolatile.isMobilePhoneNoVerified,
+        })}
+        {this.renderContentSection({
+          key: 'Email',
+          fontType: Fonts.type24,
+          value: dataMerchantVolatile.email,
+          verified: dataMerchantVolatile.isEmailVerified,
         })}
         {this.renderContentSection({
           key: 'Nomor Kartu Tanda Penduduk (KTP)',
           failed: this.checkRejectionStatus('idNo'),
-          fontType: this.checkRejectionStatus('idNo')
-            ? Fonts.type84
-            : Fonts.type24,
-          value: this.props.merchant.dataMerchantVolatile.idNo
+          fontType: this.checkRejectionStatus('idNo') ? Fonts.type84 : Fonts.type24,
+          value: dataMerchantVolatile.idNo
         })}
         {this.renderContentSection({
           key: 'Nomor Pokok Wajib Pajak (NPWP)',
           failed: this.checkRejectionStatus('taxNo'),
-          fontType: this.checkRejectionStatus('taxNo')
-            ? Fonts.type84
-            : Fonts.type24,
-          value: this.props.merchant.dataMerchantVolatile.taxNo
+          fontType: this.checkRejectionStatus('taxNo') ? Fonts.type84 : Fonts.type24,
+          value: dataMerchantVolatile.taxNo
         })}
         {this.renderContentSection({
           key: 'Foto Nomor Pokok Wajib Pajak (NPWP)',
           failed: this.checkRejectionStatus('taxImageUrl'),
-          fontType: this.checkRejectionStatus('taxImageUrl')
-            ? Fonts.type84
-            : Fonts.type98
+          fontType: this.checkRejectionStatus('taxImageUrl') ? Fonts.type84 : Fonts.type98
         })}
         {this.renderContentSection({
           key: 'Foto KTP',
           failed: this.checkRejectionStatus('idImageUrl'),
-          fontType: this.checkRejectionStatus('idImageUrl')
-            ? Fonts.type84
-            : Fonts.type98,
-          value: this.props.merchant.dataMerchantVolatile.idImageUrl
-            ? 'Sudah diupload'
-            : '-'
+          fontType: this.checkRejectionStatus('idImageUrl') ? Fonts.type84 : Fonts.type98,
+          value: dataMerchantVolatile.idImageUrl ? 'Sudah diupload' : '-'
         })}
         {this.renderContentSection({
           key: 'Foto Selfie + KTP',
           failed: this.checkRejectionStatus('selfieImageUrl'),
-          fontType: this.checkRejectionStatus('selfieImageUrl')
-            ? Fonts.type84
-            : Fonts.type98,
-          value: this.props.merchant.dataMerchantVolatile.selfieImageUrl
-            ? 'Sudah diupload'
-            : '-'
+          fontType: this.checkRejectionStatus('selfieImageUrl') ? Fonts.type84 : Fonts.type98,
+          value: dataMerchantVolatile.selfieImageUrl ? 'Sudah diupload' : '-'
         })}
       </View>
     );
@@ -140,6 +155,14 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     justifyContent: 'space-between',
     flexDirection: 'row'
+  },
+  emailBadge:{
+    marginHorizontal: 8, 
+    backgroundColor: Color.fontGreen50, 
+    borderRadius: 8, 
+    paddingRight: 8,
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 });
 
