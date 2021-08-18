1718ecd0095d84eb8fc37ea77d7b7f5638c466fa
Ahmad Fakhrurozi S <haifahrul@gmail.com>
2021-08-18 16:28:50 +0700

    SFA - Add Collection List: Add layout "belum ada penagihan"

--- src/screens/sfa/SfaCollectionListView.js
+++ src/screens/sfa/SfaCollectionListView.js
@@ -26,6 +26,7 @@ import {
   sfaGetReferenceListProcess,
   sfaCollectionListLoadmoreProcess
 } from '../../state/actions';
+import SfaNoDataView from './SfaNoDataView';
 
 const SfaCollectionListView = props => {
   const dispatch = useDispatch();
@@ -264,6 +265,23 @@ const SfaCollectionListView = props => {
 
   /**
    * =======================
+   * RENDER NO DATA
+   * =======================
+   */
+  const renderNoData = () => {
+    return (
+      <View style={{ marginTop: 16 }}>
+        <SfaNoDataView
+          topText="Belum ada penagihan"
+          midText="Yuk lakukan penambahan penagihanmu."
+          bottomText=""
+        />
+      </View>
+    );
+  };
+
+  /**
+   * =======================
    * RENDER COLLECTION LIST
    * =======================
    */
@@ -310,9 +328,11 @@ const SfaCollectionListView = props => {
 
   const renderContent = () => {
     return !loadingGetReferenceList ? (
-      <>
-        <View style={{ flex: 1 }}>{renderCollectionList()}</View>
-      </>
+      <View style={{ flex: 1 }}>
+        {dataGetReferenceList.meta.total > 0
+          ? renderCollectionList()
+          : renderNoData()}
+      </View>
     ) : (
       <LoadingPage />
     );
