466fe28071654a11df488d367340fa05c8663858
ayu-khairina <ayu@sinbad.co.id>
2021-10-18 15:41:34 +0700

    SFA - integrate data collection pending

--- src/screens/merchants/dashboard-merchant/MerchantNoCollectionReason.js
+++ src/screens/merchants/dashboard-merchant/MerchantNoCollectionReason.js
@@ -4,53 +4,21 @@ import {
   Text,
   TouchableOpacity
 } from '../../../library/reactPackage';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ModalBottomMerchantNoCollectionReason from './ModalBottomMerchantNoCollectionReason';
 import MerchantCollectionReasonList from './MerchantCollectionReasonList';
+import SfaNoDataView from '../../sfa/SfaNoDataView';
+import { useSelector } from 'react-redux';
+import { ButtonSingle } from '../../../library/component';
 const MerchantNoCollectionReason = () => {
   const [isModalReasonOpen, setIsModalReasonOpen] = useState(false);
-  const reasonNotCollect = [
-    { id: 1, reason: 'Masalah AR' },
-    { id: 2, reason: 'Bayar Kunjungan Berikut' },
-    { id: 3, reason: 'Bayar Cicil' },
-    { id: 4, reason: 'Toko Tutup' },
-    { id: 5, reason: 'Toko Pindah' },
-    { id: 6, reason: 'Ganti Bisnis' }
-  ];
-  const collectionUnpaid = {
-    data: {
-      orderParcels: [
-        {
-          deliveredDate: '2021-09-03 09:09:46',
-          dueDate: '2021-09-05 09:09:46',
-          id: 1081735,
-          invoiceAmount: 139740,
-          invoiceGroupName: 'Exclusive Danone',
-          orderCode: 'S0100032305811078558',
-          orderRef: null,
-          outstandingAmount: 0,
-          paidAmount: 139740,
-          statusPayment: 'overdue'
-        },
-        {
-          deliveredDate: '2021-09-03 09:09:46',
-          dueDate: '2021-09-05 09:09:46',
-          id: 1081735,
-          invoiceAmount: 139740,
-          invoiceGroupName: 'Exclusive Danone',
-          orderCode: 'S0100032305811078558',
-          orderRef: null,
-          outstandingAmount: 0,
-          paidAmount: 139740,
-          statusPayment: 'overdue'
-        }
-      ]
-    }
-  };
-  const onPressReason = () => {
-    setIsModalReasonOpen(true);
-    console.log('modal open');
-  };
+  const [dataPostTransaction, setDataPostTransaction] = useState(null);
+  const { dataSfaCheckCollectionStatus } = useSelector(state => state.sfa);
+
+  useEffect(() => {
+    setDataPostTransaction(dataSfaCheckCollectionStatus);
+  }, []);
+  
   const renderModalBottomNotCollectReason = () => {
     return isModalReasonOpen ? (
       <ModalBottomMerchantNoCollectionReason
@@ -61,13 +29,34 @@ const MerchantNoCollectionReason = () => {
     ) : null;
   };
 
-  return (
+  /** RENDER BUTTON */
+  const renderButton = () => {
+    return (
+      <>
+        <ButtonSingle
+          onPress={() => console.log('button')}
+          title={'Simpan'}
+          borderRadius={4}
+          // disabled={isSaveDisabled || loadingSfaPostPaymentMethod}
+          // loading={loadingSfaPostPaymentMethod}
+        />
+      </>
+    );
+  };
+  return dataSfaCheckCollectionStatus ? (
     <>
       <View>
-        <MerchantCollectionReasonList dataList={collectionUnpaid} />
+        <MerchantCollectionReasonList dataList={dataSfaCheckCollectionStatus} />
+        {renderButton()}
       </View>
       {renderModalBottomNotCollectReason()}
     </>
+  ) : (
+    <SfaNoDataView
+      topText={'Belum Ada Tagihan'}
+      midText={'Yuk belanja kebutuhanmu sekarang di Sinbad'}
+      bottomText={''}
+    />
   );
 };
 
