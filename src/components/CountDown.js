41eefbe6192e66ef5c60181e7a3825d60bdbdbf1
ayu-khairina <ayu@sinbad.co.id>
2021-12-06 13:23:09 +0700

    hotifx countdowntimer

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -7,7 +7,7 @@ import {
   AppState
 } from '../library/reactPackage';
 import masterColor from '../config/masterColor.json';
-
+import { Fonts } from '../config';
 class CountDown extends Component {
   constructor(props) {
     super(props);
@@ -71,14 +71,14 @@ class CountDown extends Component {
     }
     this.setState({ appState: nextAppState });
   };
-
   /** === SEND DATA TO PARENT (PdpView) */
   toParentFunction() {
     this.props.parentFunction({
-      type: 'countdown'
+      type: 'countdown',
+      status: 'false'
     });
   }
-  /** === TIME CONVERTION === */
+
   timeConverter(time) {
     // Time from state will convert to become HH:MM:SS
     const h = Math.abs(
@@ -99,150 +99,107 @@ class CountDown extends Component {
 
     return { h, m, s };
   }
-  /** === FUNCTION STYLE === */
-  smallCountDownStyle() {
-    return [
-      [
-        this.props.fontPrimer,
-        styles.smallCountDownBox,
-        {
-          backgroundColor: this.props.backgroundColor
-        }
-      ],
-      [this.props.fontPrimer, { color: this.props.backgroundColor }]
-    ];
-  }
-  smallCountDownStyleWhite() {
-    return [
-      [
-        this.props.fontPrimer,
-        styles.smallCountDownBox,
-        {
-          backgroundColor: masterColor.fontWhite,
-          color: masterColor.fontRed50,
-          borderWidth: 1,
-          borderColor: masterColor.fontRed50
-        }
-      ],
-      [this.props.fontPrimer, { color: this.props.backgroundColor }]
-    ];
-  }
   /**
    * ========================
    * RENDER VIEW
    * =======================
    */
-  /** === RENDER COUNTDOWN SMALL === */
-  renderTimeCountDownSmall() {
+
+  renderCountDown() {
+    return (
+      <View style={{ flexDirection: 'row', alignItems: 'flex-end' }}>
+        {this.props.type === 'big'
+          ? this.renderTimeCountDownBig()
+          : this.renderTimeCountDownSmallRed()}
+      </View>
+    );
+  }
+
+  renderTimeCountDownSmallRed() {
     const { h, m, s } = this.timeConverter(this.state.expiredTime);
     if (h + m + s === 0) {
       this.toParentFunction();
     }
     return (
       <View style={{ flexDirection: 'row' }}>
-        <Text style={this.smallCountDownStyle()[0]}>{h > 9 ? h : `0${h}`}</Text>
-        <Text style={this.smallCountDownStyle()[1]}>{' : '}</Text>
-        <Text style={this.smallCountDownStyle()[0]}>{m > 9 ? m : `0${m}`}</Text>
-        <Text style={this.smallCountDownStyle()[1]}>{' : '}</Text>
-        <Text style={this.smallCountDownStyle()[0]}>{s > 9 ? s : `0${s}`}</Text>
-      </View>
-    );
-  }
-  /** === RENDER COUNTDOWN BIG === */
-  renderTimeCountDownBig() {
-    const { h, m, s } = this.timeConverter(this.state.expiredTime);
-    return (
-      <View style={{ flexDirection: 'row' }}>
-        <Text style={this.props.fontPrimer}>{h} </Text>
-        <Text style={this.props.fontSecondary}>{'Jam '}</Text>
-        <Text style={this.props.fontPrimer}>{m} </Text>
-        <Text style={this.props.fontSecondary}>{'Menit '}</Text>
-        <Text style={this.props.fontPrimer}>{s} </Text>
-        <Text style={this.props.fontSecondary}>{'Detik'}</Text>
-      </View>
-    );
-  }
-  /** === RENDER COUNTDOWN TEXT ONLY === */
-  renderTimeCountDownTextOnly() {
-    const { h, m } = this.timeConverter(this.state.expiredTime);
-    return (
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Text style={this.props.fontPrimer}>{h}</Text>
-        <Text style={this.props.fontPrimer}>{'Jam '}</Text>
-        <Text style={this.props.fontPrimer}>{m}</Text>
-        <Text style={this.props.fontPrimer}>{'menit'}</Text>
-      </View>
-    );
-  }
-  /** === RENDER COUNTDOWN NUMBER === */
-  renderTimeCountDownNumver() {
-    const { h, m, s } = this.timeConverter(this.state.expiredTime);
-    if (h + m + s === 0) {
-      this.toParentFunction();
-    }
-    return (
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Text style={this.props.fontPrimer}>{h > 9 ? h : `0${h}`}</Text>
-        <Text style={this.props.fontPrimer}>{':'}</Text>
-        <Text style={this.props.fontPrimer}>{m > 9 ? m : `0${m}`}</Text>
-        <Text style={this.props.fontPrimer}>{':'}</Text>
-        <Text style={this.props.fontPrimer}>{s > 9 ? s : `0${s}`}</Text>
-      </View>
-    );
-  }
-  /** RENDER COUNTDOWN SMALL WHITE */
-  renderTimeCountDownSmallWhite() {
-    const { h, m, s } = this.timeConverter(this.state.expiredTime);
-    if (h + m + s === 0) {
-      this.toParentFunction();
-    }
-    return (
-      <View style={{ flexDirection: 'row' }}>
-        <Text style={this.smallCountDownStyleWhite()[0]}>
+        <Text
+          style={{ ...styles.countDownText, ...styles.countDownTextContainer }}
+        >
           {h > 9 ? h : `0${h}`}
         </Text>
-        <Text style={this.smallCountDownStyleWhite()[1]}>{' : '}</Text>
-        <Text style={this.smallCountDownStyleWhite()[0]}>
+        <Text style={{ ...styles.countDownText, color: masterColor.fontRed50 }}>
+          {' : '}
+        </Text>
+        <Text
+          style={{ ...styles.countDownText, ...styles.countDownTextContainer }}
+        >
           {m > 9 ? m : `0${m}`}
         </Text>
-        <Text style={this.smallCountDownStyleWhite()[1]}>{' : '}</Text>
-        <Text style={this.smallCountDownStyleWhite()[0]}>
+        <Text style={{ ...styles.countDownText, color: masterColor.fontRed50 }}>
+          {' : '}
+        </Text>
+        <Text
+          style={{ ...styles.countDownText, ...styles.countDownTextContainer }}
+        >
           {s > 9 ? s : `0${s}`}
         </Text>
       </View>
     );
   }
-  /** === RENDER CONTENT === */
-  renderContent() {
-    switch (this.props.type) {
-      case 'big':
-        return this.renderTimeCountDownBig();
-      case 'small':
-        return this.renderTimeCountDownSmall();
-      case 'textOnly':
-        return this.renderTimeCountDownTextOnly();
-      case 'number':
-        return this.renderTimeCountDownNumver();
-      case 'smallWhite':
-        return this.renderTimeCountDownSmallWhite();
-      default:
-        break;
-    }
+
+  renderTimeCountDownBig() {
+    const { h, m, s } = this.timeConverter(this.state.expiredTime);
+    return (
+      <View style={styles.countDownContainer}>
+        <Text style={styles.countDownTextBold}>{h} </Text>
+        <Text style={styles.countDownTextRegular}>{'Jam '}</Text>
+        <Text style={styles.countDownTextBold}>{m} </Text>
+        <Text style={styles.countDownTextRegular}>{'Menit '}</Text>
+        <Text style={styles.countDownTextBold}>{s} </Text>
+        <Text style={styles.countDownTextRegular}>{'Detik'}</Text>
+      </View>
+    );
   }
-  /** === MAIN === */
+
   render() {
-    return this.renderContent();
+    return this.renderCountDown();
   }
 }
 
 const styles = StyleSheet.create({
-  smallCountDownBox: {
+  countDownText: {
+    fontFamily: Fonts.MontserratSemiBold,
+    fontSize: 11,
+    lineHeight: 14,
+    color: masterColor.fontWhite
+  },
+  countDownTextContainer: {
+    backgroundColor: masterColor.fontRed50,
     borderRadius: 2,
-    paddingVertical: 2,
-    minWidth: 22,
+    minWidth: 20,
     textAlign: 'center',
-    alignItems: 'center',
+    alignContent: 'center',
     justifyContent: 'center'
+  },
+  countDownContainer: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    marginBottom: 10
+  },
+  countDownTextBold: {
+    color: masterColor.fontBlack50,
+    fontFamily: Fonts.MontserratBold,
+    fontSize: 18
+  },
+  countDownTextRegular: {
+    color: masterColor.fontBlack50,
+    fontFamily: Fonts.MontserratMedium,
+    fontSize: 14
+  },
+  headerCountdownContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center'
   }
 });
 
