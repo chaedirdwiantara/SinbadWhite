032f8701782ab5a75d4346ac92efe1d36cd92800
ayu-khairina <ayu@sinbad.co.id>
2022-01-25 13:47:59 +0700

    add integrate API get merchant credit limit list

--- src/state/actions/MerchantAction.js
+++ src/state/actions/MerchantAction.js
@@ -806,3 +806,30 @@ export function getRadiusLockGeotagSuccess(data) {
 export function getRadiusLockGeotagFailed(data) {
   return { type: types.GET_RADIUS_LOCK_GEOTAG_FAILED, payload: data };
 }
+/**
+ * ============================
+ * GET MERCHANT CREDIT LIMIT LIST
+ * ============================
+ */
+export function merchantGetCreditLimitListProcess(data) {
+  return { type: types.MERCHANT_GET_CREDIT_LIMIT_PROCESS, payload: data };
+}
+export function merchantGetCreditLimitListSuccess(data) {
+  if (data.result === 'Ok') {
+    return {
+      type: types.MERCHANT_GET_CREDIT_LIMIT_SUCCESS,
+      payload: data.data
+    };
+  }
+  return { type: types.MERCHANT_GET_CREDIT_LIMIT_FAILED, payload: data };
+}
+export function merchantGetCreditLimitListFailed(data) {
+  return { type: types.MERCHANT_GET_CREDIT_LIMIT_FAILED, payload: data };
+}
+export function merchantGetCreditLimitListReset() {
+  return { type: types.MERCHANT_GET_CREDIT_LIMIT_RESET };
+}
+/** === LOAD MORE GET MERCHANT  CREDIT LIMIT LIST=== */
+export function merchantGetCreditLimitListLoadMore(page) {
+  return { type: types.MERCHANT_GET_CREDIT_LIMIT_LOADMORE, payload: page };
+}
