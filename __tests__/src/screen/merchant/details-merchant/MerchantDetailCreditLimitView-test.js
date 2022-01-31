550bad84a66535b6ae0789c978d13bb685e68dfe
ayu-khairina <ayu@sinbad.co.id>
2022-01-31 17:35:15 +0700

    add unit test for mechant detail credit limit view

--- __tests__/src/screen/merchant/details-merchant/MerchantDetailCreditLimitView-test.js
+++ __tests__/src/screen/merchant/details-merchant/MerchantDetailCreditLimitView-test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import MerchantDetailCreditLimitView from '../../../../../src/screens/merchants/details-merchant/MerchantDetailCreditLimitView';
 const middlewares = [];
 const mockStore = configureStore(middlewares);
 describe('DETAIL CREDIT LIMIT', function() {
   const mockCreditLimitState = {
     merchant: {
+      dataGetMerchantDetailV2: {
+        supplier: {
+          id: 1
+        },
+        store: { id: 202 }
+      },
+      refreshGetCreditLimitList: false,
       loadingMerchantCreditLimitSummary: false,
       dataGetCreditLimitSummary: {
         balanceAmount: 2658369,
@@ -50,12 +57,31 @@ describe('DETAIL CREDIT LIMIT', function() {
       ...mockCreditLimitState,
       ...attr
     });
-  test('render flatlist', () => {
+  test('if component length of the list same with data', () => {
     const store = factoryMockStore({});
-    const { getByTestId } = render(
+    const { getAllByTestId } = render(
       <Provider store={store}>
         <MerchantDetailCreditLimitView data={mockCreditLimitState.merchant} />
       </Provider>
     );
+    //find the component list
+    const invoiceList = getAllByTestId('view-invoice-list');
+
+    //expect component list to be same as mock data
+    expect(invoiceList.length).toBe(2);
+  });
+  test('if invoice detail info show on press button see more', () => {
+    const store = factoryMockStore({});
+    const { getAllByTestId } = render(
+      <Provider store={store}>
+        <MerchantDetailCreditLimitView data={mockCreditLimitState.merchant} />
+      </Provider>
+    );
+    //find component button see more
+    const btnSeeMore = getAllByTestId('btn-see-more');
+    // Expect invoice info expanded on press button;
+    fireEvent.press(btnSeeMore[1]);
+    const invoiceFullInfo = getAllByTestId('view-full-invoice');
+    expect(invoiceFullInfo).toBeDefined();
   });
 });
