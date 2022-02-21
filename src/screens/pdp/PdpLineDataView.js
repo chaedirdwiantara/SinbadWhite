5ae899a323a39452db6859421847174cb8b71ea5
ayu-khairina <ayu@sinbad.co.id>
2022-02-21 15:14:32 +0700

    fix missing invoice data check credit limit

--- src/screens/pdp/PdpLineDataView.js
+++ src/screens/pdp/PdpLineDataView.js
@@ -63,7 +63,8 @@ class PdpLineDataView extends Component {
         onPress={() =>
           this.toParentFunction({
             type: item.isBundle ? 'goToBundlePage' : 'openModalOrder',
-            data: item.id
+            data: item.id,
+            invoice: item.brand.invoiceGroupBrands
           })
         }
         style={[styles.pesanButton, { backgroundColor: Color.mainColor }]}
